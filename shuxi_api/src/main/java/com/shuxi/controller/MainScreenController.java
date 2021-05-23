59c83757cda6444db6434f6d199e1cf050f16507
于浩 <2052262876@qq.com>
2021-05-24 00:20:00 +0800

    流域接口更新，船闸街口未更新

--- shuxi_api/src/main/java/com/shuxi/controller/MainScreenController.java
+++ shuxi_api/src/main/java/com/shuxi/controller/MainScreenController.java
@@ -1,14 +1,21 @@
 package com.shuxi.controller;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.shuxi.entity.TdmShipTonnageDistributionDf;
+import com.shuxi.entity.TdmXjMainLineFreightRateDf;
 import com.shuxi.service.ITdmShipTonnageDistributionDfService;
+import com.shuxi.service.ITdmXjMainLineFreightRateDfService;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -22,6 +29,8 @@ import java.util.List;
 public class MainScreenController {
     @Autowired
     private ITdmShipTonnageDistributionDfService tdmShipTonnageDistributionDfService;
+    @Autowired
+    private ITdmXjMainLineFreightRateDfService tdmXjMainLineFreightRateDfService;
     //西江流域（近一月）船舶吨位分布图
     @GetMapping("/valleyShipTonDistributing")
     public String valleyShipTonDistributing() {
@@ -49,6 +58,48 @@ public class MainScreenController {
             try {
                 jsonObject.put("success",false);
                 jsonObject.put("message","获取数据失败");
+            } catch (JSONException jsonException) {
+
+            }
+            return jsonObject.toString();
+        }
+    }
+    //运力周指数(流域)
+    @RequestMapping("/capacityWeekIndexByValley")
+    public String capacityWeekIndexByValley(@RequestParam String valley){
+        QueryWrapper<TdmXjMainLineFreightRateDf> queryWrapper = new QueryWrapper<TdmXjMainLineFreightRateDf>().eq("target", "流域").eq("targe_name",valley);
+        List<TdmXjMainLineFreightRateDf> tdmXjMainLineFreightRateDfs = tdmXjMainLineFreightRateDfService.list(queryWrapper);
+        tdmXjMainLineFreightRateDfs.sort(new Comparator<TdmXjMainLineFreightRateDf>() {
+            @Override
+            public int compare(TdmXjMainLineFreightRateDf o1, TdmXjMainLineFreightRateDf o2) {
+                return o1.getMonthDate().compareTo(o2.getMonthDate());
+            }
+        });
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("success",true);
+            JSONArray jsonArray = new JSONArray();
+            JSONArray jsonArray1 = new JSONArray();
+            jsonArray1.put("流域");
+            jsonArray1.put("年月");
+            jsonArray1.put("上行指数");
+            jsonArray1.put("下行指数");
+            jsonArray.put(jsonArray1);
+            for (TdmXjMainLineFreightRateDf tdmXjMainLineFreightRateDf : tdmXjMainLineFreightRateDfs) {
+                JSONArray jsonArray2 = new JSONArray();
+                jsonArray2.put(tdmXjMainLineFreightRateDf.getTargetName());
+                jsonArray2.put(tdmXjMainLineFreightRateDf.getMonthDate());
+                jsonArray2.put(Double.parseDouble(tdmXjMainLineFreightRateDf.getUpRate()));
+                jsonArray2.put(Double.parseDouble(tdmXjMainLineFreightRateDf.getDownRate()));
+                jsonArray.put(jsonArray2);
+            }
+            jsonObject.put("content",jsonArray);
+            return jsonObject.toString();
+        } catch (Exception e) {
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("success",false);
+                jsonObject.put("message","数据获取失败");
             } catch (JSONException jsonException) {
 
             }
