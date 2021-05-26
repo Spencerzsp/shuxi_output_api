ce4ac4591aa8d22a37099e2fe7c0a4efd035996a
于浩 <2052262876@qq.com>
2021-05-26 12:02:01 +0800

    更新副屏接口

---
+++ shuxi_api/src/main/java/com/shuxi/controller/ThirdScreenController.java
@@ -0,0 +1,16 @@
+package com.shuxi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @Author: Yu Hao
+ * DateTime: 2021-05-26 11:50
+ */
+@RestController
+public class ThirdScreenController {
+    //2021年西江流域十佳发货地
+
+}
